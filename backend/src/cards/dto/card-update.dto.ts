dbc1c70da360b21e915542d6c7e1a42639244944
Denys Harkovenko <dgarkovenko10@gmail.com>
2026-01-28 22:49:07 +0200

    Fixed bugs on back

--- backend/src/cards/dto/card-update.dto.ts
+++ backend/src/cards/dto/card-update.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateCardRequest {
   @IsOptional()
@@ -7,6 +7,5 @@ export class UpdateCardRequest {
 
   @IsOptional()
   @IsString({ message: 'Description should be a string' })
-  @IsNotEmpty({ message: 'Title cannot be empty' })
   description?: string;
 }
